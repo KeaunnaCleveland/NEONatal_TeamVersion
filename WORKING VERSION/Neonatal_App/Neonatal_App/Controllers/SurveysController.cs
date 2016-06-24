e1ebaf1198c5e1e28020d4e234c8f8e160d74cd5
michaelnader <micnad@sbcglobal.net>
2016-06-24 08:30:15 -0400

    updates

--- WORKING VERSION/Neonatal_App/Neonatal_App/Controllers/SurveysController.cs
+++ WORKING VERSION/Neonatal_App/Neonatal_App/Controllers/SurveysController.cs
@@ -17,8 +17,29 @@ namespace Neonatal_App.Controllers
         // GET: Surveys
         public ActionResult Index()
         {
-            var surveys = db.Surveys.Include(s => s.Client);
-            return View(surveys.ToList());
+            var surveys = new List<Survey>();
+            /*var selectSurvey = from m in db.Surveys
+                               where m.client_id == m.Client.id
+                               select m;*/
+            return View(db.Surveys.ToList());
+
+        }
+
+        //GET Surveys/RiskScore/5
+
+        public ActionResult RiskScore(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Survey survey = db.Surveys.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(survey);
         }
 
         // GET: Surveys/Details/5
@@ -53,9 +74,136 @@ namespace Neonatal_App.Controllers
         {
             if (ModelState.IsValid)
             {
-                db.Surveys.Add(survey);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                // ENTER RISK SCORE CODE HERE!!!
+
+                //QUESTION 1 - RACE
+                int risk_Score = 0;
+
+                int race = Convert.ToInt32(survey.Q1_race);
+
+                if (race == 1)
+                {
+                    risk_Score += 2;
+                }
+                else
+                {
+                    risk_Score += 0;
+                }
+                //QUESTION 2 - Ward
+                int ward = Convert.ToInt32(survey.Q2_ward);
+                {
+                    if (ward == 1)
+                    {
+                        risk_Score += 53;
+                    }
+                    else if (ward == 2)
+                    {
+                        risk_Score += 17; ;
+                    }
+                    else if (ward == 3)
+                    {
+                        risk_Score += 18;
+                    }
+                    else if (ward == 4)
+                    {
+                        risk_Score += 16;
+                    }
+                    else if (ward == 5)
+                    {
+                        risk_Score += 37;
+                    }
+                    else if (ward == 6)
+                    {
+                        risk_Score += 15;
+                    }
+                    else if (ward == 7)
+                    {
+                        risk_Score += 19;
+                    }
+                    else if (ward == 8)
+                    {
+                        risk_Score += 15;
+                    }
+                    else if (ward == 9)
+                    {
+                        risk_Score += 33;
+                    }
+                    else if (ward == 10)
+                    {
+                        risk_Score += 32;
+                    }
+                    else if (ward == 11)
+                    {
+                        risk_Score += 20;
+                    }
+                    else if (ward == 12)
+                    {
+                        risk_Score += 21;
+                    }
+                    else if (ward == 13)
+                    {
+                        risk_Score += 16;
+                    }
+                    else if (ward == 14)
+                    {
+                        risk_Score += 21;
+                    }
+                    else if (ward == 15)
+                    {
+                        risk_Score += 38;
+                    }
+                    else if (ward == 16)
+                    {
+                        risk_Score += 31;
+                    }
+                    else if (ward == 17)
+                    {
+                        risk_Score += 10;
+                    }
+
+                    // QUESTION 3 - FIRST CHILD
+                    int first_child = Convert.ToInt32(survey.Q3_first_child);
+
+                    if (first_child == 1)
+                    {
+                        risk_Score += 5;
+                    }
+                    else
+                    {
+                        risk_Score += 0;
+                    }
+
+                    // QUESTION 4 - PREMATURE BIRTH
+                    int prem_birth = Convert.ToInt32(survey.Q4_prem_birth);
+
+                    if (prem_birth == 1)
+                    {
+                        risk_Score += 5;
+                    }
+                    else
+                    {
+                        risk_Score += 0;
+                    }
+
+                    // QUESTION 5 - SELF-ALCOHOL
+                    int self_alcohol = Convert.ToInt32(survey.Q10_alcohol);
+
+                    if (self_alcohol == 1)
+                    {
+                        risk_Score += 4;
+                    }
+                    else
+                    {
+                        risk_Score += 0;
+                    }
+
+                    //calculate risk score
+                    survey.risk_score += risk_Score;
+
+                    db.Surveys.Add(survey);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
             }
 
             ViewBag.client_id = new SelectList(db.Clients, "id", "first_name", survey.client_id);
@@ -88,25 +236,136 @@ namespace Neonatal_App.Controllers
             if (ModelState.IsValid)
             {
                 // ENTER RISK SCORE CODE HERE!!!
-                int risk_score = 0;
+
+                //QUESTION 1 - RACE
+                int risk_Score = 0;
 
                 int race = Convert.ToInt32(survey.Q1_race);
 
                 if (race == 1)
                 {
-                    risk_score += 2;
+                    risk_Score += 2;
                 }
                 else
                 {
-                    risk_score += 0;
-
-                    survey.risk_score = risk_score;
+                    risk_Score += 0;
                 }
+                //QUESTION 2 - Ward
+                int ward = Convert.ToInt32(survey.Q2_ward);
+                {
+                    if (ward == 1)
+                    {
+                        risk_Score += 53;
+                    }
+                    else if (ward == 2)
+                    {
+                        risk_Score += 17; ;
+                    }
+                    else if (ward == 3)
+                    {
+                        risk_Score += 18;
+                    }
+                    else if (ward == 4)
+                    {
+                        risk_Score += 16;
+                    }
+                    else if (ward == 5)
+                    {
+                        risk_Score += 37;
+                    }
+                    else if (ward == 6)
+                    {
+                        risk_Score += 15;
+                    }
+                    else if (ward == 7)
+                    {
+                        risk_Score += 19;
+                    }
+                    else if (ward == 8)
+                    {
+                        risk_Score += 15;
+                    }
+                    else if (ward == 9)
+                    {
+                        risk_Score += 33;
+                    }
+                    else if (ward == 10)
+                    {
+                        risk_Score += 32;
+                    }
+                    else if (ward == 11)
+                    {
+                        risk_Score += 20;
+                    }
+                    else if (ward == 12)
+                    {
+                        risk_Score += 21;
+                    }
+                    else if (ward == 13)
+                    {
+                        risk_Score += 16;
+                    }
+                    else if (ward == 14)
+                    {
+                        risk_Score += 21;
+                    }
+                    else if (ward == 15)
+                    {
+                        risk_Score += 38;
+                    }
+                    else if (ward == 16)
+                    {
+                        risk_Score += 31;
+                    }
+                    else if (ward == 17)
+                    {
+                        risk_Score += 10;
+                    }
 
-                db.Entry(survey).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                    // QUESTION 3 - FIRST CHILD
+                    int first_child = Convert.ToInt32(survey.Q3_first_child);
+
+                    if (first_child == 1)
+                    {
+                        risk_Score += 5;
+                    }
+                    else
+                    {
+                        risk_Score += 0;
+                    }
+
+                    // QUESTION 4 - PREMATURE BIRTH
+                    int prem_birth = Convert.ToInt32(survey.Q4_prem_birth);
+
+                    if (prem_birth == 1)
+                    {
+                        risk_Score += 5;
+                    }
+                    else
+                    {
+                        risk_Score += 0;
+                    }
+
+                    // QUESTION 5 - SELF-ALCOHOL
+                    int self_alcohol = Convert.ToInt32(survey.Q10_alcohol);
+
+                    if (self_alcohol == 1)
+                    {
+                        risk_Score += 4;
+                    }
+                    else
+                    {
+                        risk_Score += 0;
+                    }
+
+                    //calculate risk score total
+                    survey.risk_score += risk_Score;
+                    db.Entry(survey).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
             }
+
             ViewBag.client_id = new SelectList(db.Clients, "id", "first_name", survey.client_id);
             return View(survey);
         }
